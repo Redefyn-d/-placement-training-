6aabf842fc12bdbbdc5fb0d3e8004b53b33da913
Redefyn-d <148458558+Redefyn-d@users.noreply.github.com>
2024-07-28 17:56:11 +0530

    Create sort an array in desc order .py

---
+++ 28,07.2024/sort an array in desc order .py
@@ -0,0 +1,12 @@
+a=int(input())
+lst=[]
+for i in range(a):
+    x=int(input())
+    lst.append(x)
+for i in range(a):
+    for j in range(i+1,a):
+        if lst[i]<lst[j]:
+            temp=lst[i]
+            lst[i]=lst[j]
+            lst[j]=temp
+print(lst)
