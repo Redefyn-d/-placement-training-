afbd83b44d5bde8317256def63517b777a4ab9ce
Redefyn-d <148458558+Redefyn-d@users.noreply.github.com>
2024-07-29 21:37:50 +0530

    Create increment.c

---
+++ 29.07.2024/increment.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void add(int x){
+  x+=1;
+  printf("Increemented",x)
+}
+int main(){
+    int x;
+    printf("Enter numbers:");
+    scanf("%d",&x);
+    printf("Before increement:%d",x);
+    add(x);
+    printf("After increement:%d",x);
+}
