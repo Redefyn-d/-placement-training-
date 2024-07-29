79070afe9ee22df51f421060a00b6ae35b8b2d1d
Redefyn-d <148458558+Redefyn-d@users.noreply.github.com>
2024-07-29 20:46:04 +0530

    Create check prime number.c

---
+++ 29.07.2024/check prime number.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int prime_check(int a){
+    int c=0;
+    for (int i=1;i<=a;i++){
+        if (i%a==0){
+            c+=1;
+        }
+    }
+    if (c==2){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+int main(){
+    int x;
+    scanf("%d",&x);
+    if (prime_check(x)){
+        printf("Prime Number");
+    }
+    else{
+        printf("Not Prime Number");
+    }
+}
