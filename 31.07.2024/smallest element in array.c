2b7d6d3afb319ff7be7cf9cc6af77b59e870cd85
Redefyn-d <148458558+Redefyn-d@users.noreply.github.com>
2024-07-31 20:39:27 +0530

    Create smallest element in array.c

---
+++ 31.07.2024/smallest element in array.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main() {
+    int n;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int min = arr[0];
+    for (int i = 1; i < n; i++) {
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+    }
+    printf("The smallest element is: %d\n", min);
+    return 0;
+}
