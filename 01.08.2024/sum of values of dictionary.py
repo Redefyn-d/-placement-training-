e416353d5635046ccc09c5b27e8949863f555375
Redefyn-d <148458558+Redefyn-d@users.noreply.github.com>
2024-08-01 20:48:51 +0530

    Create sum of values of dictionary.py

---
+++ 01.08.2024/sum of values of dictionary.py
@@ -0,0 +1,9 @@
+dict={}
+lst=[]
+for i in range(4):
+    name=input('Enter the name of the person')
+    age=int(input('Enter the age of the person'))
+    lst.append([name,age])
+for i in lst:
+    dict[i[0]]=i[1]
+print(sum(dict.values()))
