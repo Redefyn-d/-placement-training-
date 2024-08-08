14f6449ba584274de97703f933d1d4e58f159764
Redefyn-d <148458558+Redefyn-d@users.noreply.github.com>
2024-08-08 18:38:05 +0530

    Create find even elements in list.py

---
+++ 08.08.2024/find even elements in list.py
@@ -0,0 +1,12 @@
+def even_check(lst):
+    for i in lst:
+        if i%2==0:
+            print(i)
+        else:
+            continue
+lst=[]
+a=int(input("Enter the number of elements in lst"))
+for i in range(a):
+    x=int(input("Enter the value of element: "))
+    lst.append(x)
+even_check(lst)
